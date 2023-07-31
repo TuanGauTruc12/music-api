4e654055777c8a820a5b11d6c4df5286401a306f
TuanGauTruc12 <phamtuan12022001@gmail.com>
2023-07-31 09:19:17 +0700

    reset index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ import { ZingMp3 } from "zingmp3-api-full";
 dotenv.config();
 const server = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
 server.use(cors());
 
