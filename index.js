e362853694b0e4af8f19bd998e7889f74ddf8c5c
TuanGauTruc12 <phamtuan12022001@gmail.com>
2023-07-31 17:34:56 +0700

    edit vercel.json

--- index.js
+++ index.js
@@ -4,28 +4,12 @@ import dotenv from "dotenv";
 import { ZingMp3 } from "zingmp3-api-full";
 
 dotenv.config();
+const PORT = process.env.PORT || 8080;
 const server = express();
-
 server.use(cors());
-const allowedOrigins = ["http://localhost:5173"];
-
-server.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-  }
-  next();
-});
-
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 8080;
+
 
 server.get("/:id", (req, res) => {
   ZingMp3.getSong(req.params.id).then((data) => {
@@ -42,6 +26,7 @@ server.get("/playlist/:id", (req, res) => {
 server.get("/", (req, res) => {
   ZingMp3.getHome().then((value) => res.json(value.data));
 });
+
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
