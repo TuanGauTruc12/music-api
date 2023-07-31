eb146308dc0efd33a2e16bb0b66e8a2cfdf149cb
TuanGauTruc12 <phamtuan12022001@gmail.com>
2023-07-31 10:12:29 +0700

    add cors

--- index.js
+++ index.js
@@ -8,13 +8,21 @@ const server = express();
 
 server.use(cors());
 
-server.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+const allowedOrigins = ["http://localhost:5173"];
+
+server.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+  }
   next();
 });
-
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
